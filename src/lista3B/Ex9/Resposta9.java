4e14dd2cdbf69e7ad744b16cf49f791a82ff7d81
cast10 <castanha.douglas2013@gmail.com>
2022-06-28 21:55:04 -0300

    Resposta da Lista 3 B Ex9 Feito

---
+++ src/lista3B/Ex9/Resposta9.java
@@ -0,0 +1,26 @@
+package src.lista3B.Ex9;
+
+import java.util.Scanner;
+
+public class Resposta9 {
+
+    public static void main(String[] args){
+
+        Scanner numberfator = new Scanner(System.in);
+        System.out.println("Digito um numero");
+        int numero = numberfator.nextInt();
+
+        int cont = numero;
+        int soma = 1;
+
+        while (cont > 1){
+            soma *= cont;
+            cont --;
+
+
+        }
+
+        System.out.println(" A resposta é " + numero +  " é igual o  " + soma );
+    }
+
+}

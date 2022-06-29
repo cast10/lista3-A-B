efa967f26fe63e20656560a9631ae3adc4e22ee3
cast10 <castanha.douglas2013@gmail.com>
2022-06-28 21:50:22 -0300

    Resposta da Lista 3 B Ex8 Feito

---
+++ src/lista3B/Ex8/Resposta8.java
@@ -0,0 +1,20 @@
+package src.lista3B.Ex8;
+
+//8. Faça um programa que calcule e mostre a soma dos 50 primeiros números ímpares.//
+
+public class Resposta8 {
+    public static void main(String[] args){
+
+        int contador = 0;
+
+        for (int i = 1; i <= 100; i++){
+            if (i % 2 != 0){
+            contador += i;
+
+        }
+
+    } System.out.println(" valor dos números impares é  intervalos é  "  +contador);
+    }
+
+}
+
